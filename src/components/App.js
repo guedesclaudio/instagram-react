f064a8cffac958d9b39dcff6b745c33e5051d423
guedesclaudio <cgsarmentosilva@gmail.com>
2022-07-14 11:57:18 -0300

    Primeiro commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import ReactDom from 'react-dom'
+
+//importar todos os components
+import Navbar from './Navbar'
+
+export default function App() {
+    return (
+        <div>
+            <Navbar/>
+        </div>
+    )
+}
